0bbb60f72e7d622e75efd1034f0ca4e979a015cf
mdshorifulislam124 <asunhadispori11@gmail.com>
2026-04-29 13:32:20 +0600

    grade point

---
+++ grade.js
@@ -0,0 +1,20 @@
+let point = 90;
+
+if (point >= 90 && point <= 100){
+    console.log("Grade A+");
+}
+else if (point >= 80 && point <= 89){
+    console.log("Grade A");
+}
+else if (point >= 70 && point <= 79){
+    console.log("Grade B")
+}
+else if (point >= 60 && point <= 69){
+    console.log("Grade C");
+}
+else if (point >= 33 && point <= 59){
+    console.log("Grade D");
+}
+else {
+    console.log("Grade F")
+}
